9fadf15e2f9e5ea178c9337016947097a9fa459d
Meric BAL <MB@Meric-MacBook-Air.local>
2017-01-31 14:48:36 -0800

    Added posts index and routes.

--- example/app/controllers/posts_controller.rb
+++ example/app/controllers/posts_controller.rb
@@ -1,15 +1,30 @@
 class PostsController < ApplicationController
 
 	def index
-	end
-
-	def show
+		@posts = Post.all
 	end
 
 	def new
+		@post = Post.new
+	end
+
+	def show
+		@post = Post.find(params[:id])
 	end
 
 	def create
+		@post = Post.new(post_params)
+		if @post.save 
+			redirect_to '/'
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :body)
 	end
 
 end
--- example/config/routes.rb
+++ example/config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 	get '/' => 'apps#index'
 
 	# resources :users, only: [:index, :new, :create, :show, :edit, :update]
+	resources :posts, only: [:index]
 	resources :users do
-		resources :posts, only: [:index , :new, :create , :destroy]
+		resources :posts, only: [:index, :new, :create, :destroy]
 	end
-	resources :sessions, only: [:new, :create, :destroy]
+	resources :sessions, only: [:index, :new, :create, :destroy]
 
 end
