4b8facbd8ff1f5b9136506e24078e53e6de15d62
Meric BAL <MB@Meric-MacBook-Air.local>
2017-01-31 10:44:00 -0800

    Need fixes. But not today.

--- herokus/fallout/app/controllers/users_controller.rb
+++ herokus/fallout/app/controllers/users_controller.rb
@@ -4,4 +4,31 @@ class UsersController < ApplicationController
 		@users = User.all
 	end
 
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.valid?
+			@user.save
+			auto_login(@user)
+			redirect_to user_path(@user)
+		else
+			@errors = @user.errors.full_messages
+			render action: 'new'
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :email, :password, :password_confirmation)
+	end
+
 end
--- herokus/fallout/app/models/user.rb
+++ herokus/fallout/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
-	has_secure_password
-	validates :username, :email, presence: true
+  authenticates_with_sorcery!
+
+  validates :username, :email, presence: true, uniqueness: true
+  # validates :password, :password_confirmation, presence: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true#, if: -> { new_record? || changes[:crypted_password] 	}
 end
