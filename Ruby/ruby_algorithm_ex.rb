5aa23951c3e7a66a4c7fe1a08ea282d3ce57ce22
Meric BAL <MB@Meric-MacBook-Air.local>
2017-10-17 11:59:08 -0700

    Q 2

--- Ruby/ruby_algorithm_ex.rb
+++ Ruby/ruby_algorithm_ex.rb
@@ -10,8 +10,25 @@ end
 # p rev_str 'Hello'
 
 
+
 # Q2
-# Palidrome
+# Palindrome
 # tacocat == tacocat => true
 # asd => false
+
+# All the options would work works
+
+def palindrome input
+	if input == input.reverse then true else false end
+	# input == input.reverse ? true : false
+	# input == rev_str(input) ? 'Yes' : 'No'
+	# if input == input.reverse; true else false end
+	
+	# return input.reverse == input
+	# return rev_str(input) == input
+	# input.reverse == input
+end
+
+# p palindrome 'tacocat'
+# p palindrome 'merici'
 
