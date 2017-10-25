f30f776eb7cf9367f94f4d964caf62fc1eb155b3
Meric BAL <MB@Meric-MacBook-Air.local>
2017-10-24 19:10:49 -0700

    Change of files.

--- herokus/app/app/controllers/application_controller.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationController < ActionController::Base
-  include SessionHelper
-  protect_from_forgery with: :exception
-end
--- herokus/app/app/models/settlement.rb
+++
@@ -1,5 +0,0 @@
-class Settlement < ApplicationRecord
-  has_many :weapons
-  has_many :armors
-  has_many :junks
-end
